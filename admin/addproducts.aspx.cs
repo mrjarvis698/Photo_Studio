f3383c09c9649c55d4d6e27e749ccfd397b709fa
mrjarvis698 <dudamajaykumar59@gmail.com>
2021-03-09 20:48:25 +0530

    photo_studio: connection to add product data into data base

--- admin/addproducts.aspx.cs
+++ admin/addproducts.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Photo_Studio.admin
 {
@@ -16,7 +18,21 @@ namespace Photo_Studio.admin
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Photostudiodb"].ConnectionString);
+            conn.Open();
+            String str = "INSERT INTO PRODUCTS (productname,productdescription,productprice) VALUES ('" + AddProductNameTextBox.Text + "','" + AddProductDescriptionTextBox.Text + "','" + AddProductPriceTextBox.Text + "')";
+            SqlCommand cmd = new SqlCommand(str, conn);
+            int OBJ = Convert.ToInt32(cmd.ExecuteNonQuery());
+            if (OBJ > 0)
+            {
+                AddLabel.Text = "Sucessfully Registered";
+            }
+            else
+            {
+                AddLabel.Text = "Registration Failed";
+            }
 
+            conn.Close();
         }
     }
 }
