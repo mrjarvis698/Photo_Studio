2708fca6c3b0bad3c9794d3ae28d2322e1cb8ea3
mrjarvis698 <dudamajaykumar59@gmail.com>
2021-03-29 21:42:34 +0530

    photo_studio: stop duplicate users with same email

--- signup.aspx.cs
+++ signup.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data;
 
 namespace Photo_Studio
 {
@@ -20,40 +21,53 @@ namespace Photo_Studio
         {
             SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Photostudiodb"].ConnectionString);
             conn.Open();
-            String str = "INSERT INTO USERS (UserName,Address,Contact,Email,Password) VALUES ('" + SignupNameTextBox.Text + "','" + SignupAddressTextBox.Text + "','" + SignupContactTextBox.Text + "','" + SignupEmailTextBox.Text + "','" + SignupPasswordTextBox.Text + "')";
+            String str = "SELECT* FROM USERS WHERE Email=@email";
             SqlCommand cmd = new SqlCommand(str, conn);
-            int OBJ = Convert.ToInt32(cmd.ExecuteNonQuery());
-            if (OBJ > 0)
+            cmd.Parameters.AddWithValue("@email", SignupEmailTextBox.Text);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            int i = cmd.ExecuteNonQuery();
+            conn.Close();
+            if (dt.Rows.Count > 0)
             {
-                string query = "IF OBJECT_ID('dbo." + SignupEmailTextBox.Text + "', 'U') IS NULL ";
-                query += "BEGIN ";
-                query += "CREATE TABLE [dbo].[" + SignupEmailTextBox.Text + "] (";
-                query += "[Id] INT NOT NULL PRIMARY KEY IDENTITY,";
-                query += "[ProductName] NCHAR(20) NOT NULL,";
-                query += "[Description] NCHAR(250) NOT NULL,";
-                query += "[Price] MONEY NOT NULL,";
-                query += "[Quantity] INT NOT NULL";
-                query += ")";
-                query += " END";
-                string constr = ConfigurationManager.ConnectionStrings["Photostudiodb"].ConnectionString;
-                using (SqlConnection connn = new SqlConnection(constr))
-                {
-                    using (SqlCommand cmmd = new SqlCommand(query))
-                    {
-                        cmmd.Connection = connn;
-                        connn.Open();
-                        cmmd.ExecuteNonQuery();
-                        connn.Close();
-                    }
-                }
 
-                registrationlb.Text = "Sucessfully Registered. Please Login.";
+                registrationlb.Text = "You are already Registered.";
             }
             else
             {
-                registrationlb.Text = "Registration Failed";
+                SqlConnection connn = new SqlConnection(ConfigurationManager.ConnectionStrings["Photostudiodb"].ConnectionString);
+                connn.Open();
+                String strr = "INSERT INTO USERS (UserName,Address,Contact,Email,Password) VALUES ('" + SignupNameTextBox.Text + "','" + SignupAddressTextBox.Text + "','" + SignupContactTextBox.Text + "','" + SignupEmailTextBox.Text + "','" + SignupPasswordTextBox.Text + "')";
+                SqlCommand cmmd = new SqlCommand(strr, connn);
+                int OBJ = Convert.ToInt32(cmmd.ExecuteNonQuery());
+                if (OBJ > 0)
+                {
+                    string query = "IF OBJECT_ID('dbo." + SignupEmailTextBox.Text + "', 'U') IS NULL ";
+                    query += "BEGIN ";
+                    query += "CREATE TABLE [dbo].[" + SignupEmailTextBox.Text + "] (";
+                    query += "[Id] INT NOT NULL PRIMARY KEY IDENTITY,";
+                    query += "[ProductName] NCHAR(20) NOT NULL,";
+                    query += "[Description] NCHAR(250) NOT NULL,";
+                    query += "[Price] MONEY NOT NULL,";
+                    query += "[Quantity] INT NOT NULL";
+                    query += ")";
+                    query += " END";
+                    string constr = ConfigurationManager.ConnectionStrings["Photostudiodb"].ConnectionString;
+                    using (SqlConnection connnn = new SqlConnection(constr))
+                    {
+                        using (SqlCommand cmmmd = new SqlCommand(query))
+                        {
+                            cmmmd.Connection = connnn;
+                            connnn.Open();
+                            cmmmd.ExecuteNonQuery();
+                            connnn.Close();
+                        }
+                    }
+                    registrationlb.Text = "Sucessfully Registered. Please Login.";
+                }
+                conn.Close();
             }
-            conn.Close();
         }
     }
 }
