795d124124610da375456a6825bfed609559e7e6
mrjarvis698 <dudamajaykumar59@gmail.com>
2021-04-09 22:40:38 +0530

    photo_studio: some changes in nestedadmin.master

--- NestedAdmin.master.designer.cs
+++ NestedAdmin.master.designer.cs
@@ -22,5 +22,14 @@ namespace Photo_Studio
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder3;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
